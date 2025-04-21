d1fb32e2cb9c00f757b0ab48b73a10bce2254479
EmyRub <esme.rubi@hotmail.com>
2025-04-21 13:46:51 -0600

    auth

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,23 @@
 import { z } from 'zod'
 
+/** Auth & users */
+const authSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+    password_confirmation: z.string(),
+    token: z.string()
+})
+type Auth = z.infer<typeof authSchema>
+
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+export type ForgotPasswordForm = Pick<Auth, 'email'>
+export type RequestConfirmationCodeForm = Pick<Auth, 'email'>
+export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>
+
+
+export type ConfirmToken = Pick<Auth, 'token'>
+
 /**Tasks */
 export const taskStatusSchema = z.enum(["pending", "onHold", "inProgess", "underReview", "completed"])
 export type TaskStatus = z.infer<typeof taskStatusSchema>
