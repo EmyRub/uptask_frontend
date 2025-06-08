eebe8972608f48fb0bda04d0851507813746b8eb
EmyRub <esme.rubi@hotmail.com>
2025-06-08 12:09:31 -0600

    drag&drop

--- src/router.tsx
+++ src/router.tsx
@@ -15,6 +15,7 @@ import ProjectTeamView from './views/projects/ProjectTeamView'
 import ProfileView from './views/profile/ProfileView'
 import ChangePasswordView from './views/profile/ChangePasswordView'
 import ProfileLayout from './layouts/ProfileLayout'
+import NotFound from './views/404/NotFound'
 
 
 export default function Router() {
@@ -33,7 +34,7 @@ export default function Router() {
                         <Route path='/profile' element={<ProfileView />} />
                         <Route path='/profile/password' element={<ChangePasswordView />} />
                     </Route>
-                    
+
                 </Route>
 
                 <Route element={<AuthLayout />}>
@@ -44,7 +45,12 @@ export default function Router() {
                     <Route path='/auth/forgot-password' element={<ForgotPasswordView />} />
                     <Route path='/auth/new-password' element={<NewPasswordView />} />
                 </Route>
+
+                <Route element={<AuthLayout />}>
+                    <Route path='*' element={<NotFound />} />
+                </Route>
+
             </Routes>
-        </BrowserRouter>
+        </BrowserRouter >
     )
 }
